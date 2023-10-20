139633bec1f0b9081e5985940ba0f232a00f2118
Robert Fiske <r.fiske@csuohio.edu>
2023-10-20 11:32:00 -0400

    added basic npc movement and some initial structural changes

--- src/application/MainSceneController.java
+++ src/application/MainSceneController.java
@@ -40,15 +40,75 @@ public class MainSceneController {
 
     int number;
     Player player = new Player();
-    ArrayList<GameCharacter> npcs = new ArrayList<>();
+    //ArrayList<GameCharacter> npcs = new ArrayList<>();
     Random globalRand = new Random(1234);
+    
     public class GameCharacter
     {
         GameTile tile;
         String sprite = "debugcharacter.png";
         ImageView imgV;
-
+        int col,row;
     }
+    
+    public class Level
+    {
+        ArrayList<GameCharacter> npcs;
+        GameTile[][] tiles = new GameTile[gridSize][gridSize];
+        public Level() throws IOException
+        {
+            npcs = new ArrayList<>();
+            
+            GameTile curTile;
+            for (int i = 0; i < gridSize; i++) {
+
+            for (int j = 0; j < gridSize; j++) {
+                curTile = tiles[i][j];
+
+                if (curTile == null) {
+                    tiles[i][j] = curTile = new GameTile();
+
+                } else {
+                    System.out.println("yo");
+                }
+                Pane curPane = tiles[i][j].getPane();
+                gdPaneGame.add(curPane, j, i);
+                String path = "resources/" + tiles[i][j].getSpritePath();
+                //System.out.printf("Using: %s\n", path);
+                FileInputStream stream = new FileInputStream(path);
+                Image img = new Image(stream);
+                curTile.imgV = new ImageView(img);
+                curTile.getPane().getChildren().add(curTile.imgV);
+
+                curTile.imgV.fitWidthProperty().bind(gdPaneGame.widthProperty().divide(gridSize));
+                curTile.imgV.fitHeightProperty().bind(gdPaneGame.heightProperty().divide(gridSize));
+                stream.close();
+                
+                
+                if ((i == 5 && j == 4) || i == 3 && j == 2 )
+                {
+                    GameCharacter c1 = new GameCharacter();
+                    npcs.add(c1);
+                    c1.tile = curTile;
+                    c1.col = j;
+                    c1.row = i;
+                    stream = new FileInputStream("resources/" + c1.sprite);
+                    img = new Image(stream);
+                    c1.imgV = new ImageView(img);
+                    stream.close();
+                     c1.imgV.fitWidthProperty().bind(gdPaneGame.widthProperty().divide(gridSize));
+                c1.imgV.fitHeightProperty().bind(gdPaneGame.heightProperty().divide(gridSize));
+               
+                    curTile.getPane().getChildren().add(c1.imgV);
+                }
+
+            }
+
+        }
+        }
+    }
+    
+    
     
     public class Player extends GameCharacter{
 
@@ -79,57 +139,19 @@ public class MainSceneController {
             return (sprite);
         }
     }
-    GameTile[][] board = null;
+    //GameTile[][] board = null;
  int gridSize = 10;
-       
+       Level curLevel;
     @FXML
     public void setGrid(ActionEvent event) throws IOException {
         GameTile curTile;
-        board = new GameTile[gridSize][gridSize];
-        for (int i = 0; i < gridSize; i++) {
-
-            for (int j = 0; j < gridSize; j++) {
-                curTile = board[i][j];
-
-                if (curTile == null) {
-                    board[i][j] = curTile = new GameTile();
-
-                } else {
-                    System.out.println("yo");
-                }
-                Pane curPane = board[i][j].getPane();
-                gdPaneGame.add(curPane, j, i);
-                String path = "resources/" + board[i][j].getSpritePath();
-                //System.out.printf("Using: %s\n", path);
-                FileInputStream stream = new FileInputStream(path);
-                Image img = new Image(stream);
-                curTile.imgV = new ImageView(img);
-                curTile.getPane().getChildren().add(curTile.imgV);
-
-                curTile.imgV.fitWidthProperty().bind(gdPaneGame.widthProperty().divide(gridSize));
-                curTile.imgV.fitHeightProperty().bind(gdPaneGame.heightProperty().divide(gridSize));
-                stream.close();
-                
-                
-                if (globalRand.nextInt(0, 100) > 80)
-                {
-                    GameCharacter c1 = new GameCharacter();
-                    npcs.add(c1);
-                    c1.tile = curTile;
-                    stream = new FileInputStream("resources/" + c1.sprite);
-                    img = new Image(stream);
-                    c1.imgV = new ImageView(img);
-                    stream.close();
-                     c1.imgV.fitWidthProperty().bind(gdPaneGame.widthProperty().divide(gridSize));
-                c1.imgV.fitHeightProperty().bind(gdPaneGame.heightProperty().divide(gridSize));
-               
-                    curTile.getPane().getChildren().add(c1.imgV);
-                }
-
-            }
-
-        }
+        curLevel = new Level();
+        GameTile[][] board = curLevel.tiles;// = new GameTile[gridSize][gridSize];
+        
+         
         player.tile = board[2][1];
+        player.col = 1;
+        player.row = 2;
         curTile = player.tile;
         FileInputStream stream = new FileInputStream("resources/player.png");
 
@@ -165,7 +187,7 @@ public class MainSceneController {
 
     @FXML
     void keyPressed(KeyEvent event) {
-        if (board == null) {
+        if (curLevel == null) {
             return;
         }
         int row = GridPane.getRowIndex(player.tile.getPane());
@@ -179,7 +201,7 @@ public class MainSceneController {
             }
             newRow--;
         } else if (event.getCode() == KeyCode.DOWN) {
-            if (row == gridSize) {
+            if (row == gridSize-1) {
                 return;
             }
             newRow++;
@@ -189,14 +211,79 @@ public class MainSceneController {
             }
             newCol--;
         } else if (event.getCode() == KeyCode.RIGHT) {
-            if (col == gridSize) {
+            if (col == gridSize-1) {
                 return;
             }
             newCol++;
         }
-        GameTile swap = board[newRow][newCol];
+        GameTile swap = curLevel.tiles[newRow][newCol];
         player.tile.myPane.getChildren().remove(player.imgV);
         swap.myPane.getChildren().add(player.imgV);
         player.tile = swap;
+        player.col = newCol;
+        player.row = newRow;
+        
+        for (GameCharacter curChar : curLevel.npcs)
+        {
+            boolean useCol = globalRand.nextBoolean();
+
+            if (curChar.col == player.col || curChar.row == player.row)
+            {
+                if (Math.abs(player.col - curChar.col) == 1 || Math.abs(player.row - curChar.row) == 1)
+                {
+                    System.out.println("Attack");
+                    continue;
+                }
+                else if (curChar.col == player.col)
+                {
+                    useCol = false;
+                }
+                else if (curChar.row == player.row)
+                {
+                    //move x
+                    useCol = true;
+                }
+                
+            }
+            
+            col = newCol = curChar.col;
+            row = newRow = curChar.row;
+            if (useCol)
+            {
+                if (curChar.col > player.col)
+                {
+                    newCol--;
+                }
+                else
+                {
+                    newCol++;
+                }
+            }
+            else
+            {
+                if (curChar.row > player.row)
+                {
+                    newRow--;
+                }
+                else
+                {
+                    newRow++;
+                }
+            }
+            try
+            {
+        swap = curLevel.tiles[newRow][newCol];
+        curChar.tile.myPane.getChildren().remove(curChar.imgV);
+        swap.myPane.getChildren().add(curChar.imgV);
+        curChar.tile = swap;
+        curChar.col = newCol;
+        curChar.row = newRow;
+            }
+            catch(Exception e)
+            {
+                System.out.println("err");
+            }
+        
+        }
     }
 }
